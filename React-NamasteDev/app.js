713f39e1be7d1ace890cf1880b951649e96be13c
Puneeth Prabhu <puneethgprabhu@gmail.com>
2024-02-18 21:32:31 +0530

    initial commit

--- React-NamasteDev/app.js
+++ React-NamasteDev/app.js
@@ -1,10 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element
+const styleCard = {
+  backgroundColor: "#f0f0f0"
+};
 
-const heading = React.createElement("h1", { id: "heading" }, " Namaste React 🚀");
-const jsxHeading = <h1>Namaste React from jsx 🚀</h1>;
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf="
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestroCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/x4uyxvihmg8qa3pddkgf"
+        alt="meghana foods"
+      />
+      <h3>Meghana Foods</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestroCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<AppLayout />);
